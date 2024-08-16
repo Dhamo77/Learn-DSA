5f9b0ad784222de0e4fb4d228efc296aca9abb0d
Dhamo77 <dhamotharancr7@gamil.com>
2024-08-16 16:47:52 +0530

    implement the stack using circular list

---
+++ src/Stack_LinkedList.java
@@ -0,0 +1,63 @@
+// implement the stack using circular list
+public class Stack_LinkedList<T extends Comparable<T>> {
+    Node<T> head;
+    private int size;
+    private static class Node<T>{
+        T value;
+         Node<T> next;
+        public Node(T value){
+            this.value=value;
+            this.next=null;
+        }
+    }
+    public void push(T value){
+        Node<T> newnode =new Node<>(value);
+        if (head == null) {
+           head=newnode;
+           head.next=head;
+        }
+       else {
+          Node<T> current =head;
+          while (current.next!=head){
+              current=current.next;
+          }
+          newnode.next=head;
+          head=newnode;
+          current.next=head;
+        }
+
+        size++;
+    }
+    public T peek(){
+        if (head==null){
+            return null;
+        }
+        return head.value;
+    }
+    public T pop(){
+        if (head==null){
+            return null;
+        }
+        T value= head.value;
+        Node<T> current =head;
+        while (current.next!=head){
+            current=current.next;
+        }
+        head = head.next;
+        current.next=head;
+        size--;
+        return value;
+    }
+    public  int size(){
+        return size;
+    }
+    public void display(){
+        Node<T> current =head;
+        System.out.print("[ ");
+        do {
+            System.out.print(current.value+" ");
+            current=current.next;
+        }while (current!=head);
+        System.out.println("]");
+    }
+}
